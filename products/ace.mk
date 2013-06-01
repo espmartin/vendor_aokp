35780fc9d6a32ab89f763bf2a5c99afbfb8a3353
Martin Espericueta <espmartin@gmail.com>
2013-06-01 10:12:20 -0600

    Update ace.mk

--- products/ace.mk
+++ products/ace.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := htc_wwe
 PRODUCT_MODEL := Desire HD
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace TARGET_DEVICE="ace" BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
 
 # boot animation
 PRODUCT_COPY_FILES += \
