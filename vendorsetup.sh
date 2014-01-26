d76967afa2c08b70023e7096f73377bacc611f2b
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-01-26 00:59:55 -0800

    add ace
    Change-Id: I0505e2c1378fe226fa32b78c3b00f4d4b515602e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_ace-userdebug
 add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
